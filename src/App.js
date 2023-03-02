912f3b8fedc843df42c53c30675ada9b52229345
sadiqsadi6008 <sadiqsadi317@gmail.com>
2023-03-02 11:07:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
+import About from './components/screens/About'
+import Contact from './components/screens/Contact'
+import Home from './components/screens/Home'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='about' element={<About/>} />
+          <Route path='contact' element={<Contact/>} />
+      </Routes>
+    </Router>
+  )
 }
 
+const Wrapper = styled.section`
+  
+`;
 export default App;
---
+++ src/components/screens/LeftNavBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LeftNavBar() {
+  return (
+    <div>LeftNavBar</div>
+  )
+}
