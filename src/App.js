221fffd54e9c7640411014d16ee79478d16c730d
sadiqsadi6008 <sadiqsadi317@gmail.com>
2023-03-02 14:36:47 +0530

    menu section compleated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import MenuBar from './components/sreens/MenuBar'
+import Helmet from 'react-helmet'
 
 function App() {
-  return <MenuBar/>
-  
-}
+  return (
+    <>
+      <Helmet>
+        <title>CosyPOS</title>
+      </Helmet>
+      <Spotlight>
+        <Router>
+          <Routes>
+            
 
+          </Routes>
+        </Router>
+      </Spotlight>
+    </>
+  )
+}
+const Spotlight = styled.section`
+  display: flex;
+`;
 export default App
---
+++ src/components/sreens/PageNotFound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageNotFound() {
+  return (
+    <div style={{width:'100%', height:"100vh"}}><img src={require("../assets/images/pagenotfound.jpeg")} style={{width:'100%', height:"100%"}} alt='pageNotFound' /></div>
+  )
+}
+
+export default PageNotFound
